d8ff0bddf171d794fd7dfbae8ef29a201bddf12f
becarevicmarko <becarevic420@gmail.com>
2020-01-22 10:58:14 +0100

    Version5

---
+++ HushPuppies_N_M/HushPuppies_N_M/Models/Dog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HushPuppies_N_M.Models
+{
+    public class Dog
+    {
+
+        //properties
+        public string Name { get; set; }
+
+        public int Expirience { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public string Informations { get; set; }
+
+        public string Description { get; set; }
+
+        //ctors 
+        public Dog() : this("", 0, DateTime.MinValue, "", "") { }
+
+        public Dog(string name, int expirience, DateTime birthdate, string informations, string description)
+        {
+            this.Name = name;
+            this.Expirience = expirience;
+            this.Birthdate = birthdate;
+            this.Informations = informations;
+            this.Description = description;
+        }
+
+    }
+}
