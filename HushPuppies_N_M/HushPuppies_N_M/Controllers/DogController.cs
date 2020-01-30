41e8aa9d134804d0c364b9abc5bfa9f8752da7a6
becarevicmarko <becarevic420@gmail.com>
2020-01-30 13:51:36 +0100

    Dog5 hinzugefügt

--- HushPuppies_N_M/HushPuppies_N_M/Controllers/DogController.cs
+++ HushPuppies_N_M/HushPuppies_N_M/Controllers/DogController.cs
@@ -21,6 +21,7 @@ namespace HushPuppies_N_M.Controllers
             Dog d2 = new Dog("/Content/images/pomeranian.jpg", "Nimi", 2, new DateTime(2019, 11, 13), "aggro", "min. 2h spazieren am Tag ");
             Dog d3 = new Dog("/Content/images/husky.jpg", "Lana", 2, new DateTime(2019, 11, 13), "ruhig", "min. 2h spazieren am Tag ");
             Dog d4 = new Dog("/Content/images/labrador.jpg", "Bobi", 2, new DateTime(2019, 11, 13), "spielerisch", "min. 2h spazieren am Tag ");
+            Dog d5 = new Dog("/Content/images/chowchow.jpg", "Bobi", 2, new DateTime(2019, 11, 13), "spielerisch", "min. 2h spazieren am Tag ");
 
             List<Dog> d = new List<Dog>();
 
@@ -28,6 +29,7 @@ namespace HushPuppies_N_M.Controllers
             d.Add(d2);
             d.Add(d3);
             d.Add(d4);
+            d.Add(d5);
 
             return View(d);
             
